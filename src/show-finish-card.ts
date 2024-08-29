7e694a67a7fdca9d0626c94ebdcbc21f4b091012
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-08-29 11:51:34 +0000

    feat: add page link to finish card

--- src/show-finish-card.ts
+++ src/show-finish-card.ts
@@ -9,17 +9,19 @@ export function showFinishCard(trials: number) {
     const today = new Date()
     const todayString = today.getMonth() + 1 + '월 ' + today.getDate() + '일'
 
-    p.appendChild(
-        document.createTextNode(
-            `${todayString} #견주기 ${trials}번째 시도에 성공했습니다. 초성이 ${todaysChosung}인 단어를 맞출 수 있을까요?`
-        )
-    )
+    const template = [
+        `${todayString} #견주기 ${trials}번째 시도에 성공했습니다.`,
+        `초성이 ${todaysChosung}인 단어를 맞출 수 있을까요?`,
+        location.origin,
+    ]
+
+    p.innerHTML = template.join('<br>')
 
     const button = document.createElement('button')
     button.appendChild(document.createTextNode('복사'))
 
     card.addEventListener('click', () => {
-        navigator.clipboard.writeText(p.textContent!)
+        navigator.clipboard.writeText(template.join('\n\n'))
         alert('복사되었습니다')
     })
 
