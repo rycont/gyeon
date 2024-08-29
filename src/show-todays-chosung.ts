edf22b87a0d2a85f62110fb52791a07f6f443860
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-08-29 11:44:05 +0000

    feat: add "오늘의 견주기: " prefix to todaysChosungElement text node

--- src/show-todays-chosung.ts
+++ src/show-todays-chosung.ts
@@ -3,6 +3,6 @@ import getChosung from './get-chosung'
 export default function showTodaysChosung(todaysWord: string) {
     const todaysChosungElement = document.getElementById('chosung')!
     todaysChosungElement.appendChild(
-        document.createTextNode(getChosung(todaysWord))
+        document.createTextNode("오늘의 견주기: "+getChosung(todaysWord))
     )
 }
