c14cffd5a57e32f5d9d33412f692d9419df0ee75
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-12-01 08:11:59 +0900

    constants.ts 업데이트

--- functions/constants.ts
+++ functions/constants.ts
@@ -1,6 +1,6 @@
 import '@std/dotenv/load'
 
-export const ANTHROPIC_MODEL_ID = 'claude-3-5-haiku-20241022'
+export const ANTHROPIC_MODEL_ID = 'claude-3-haiku-20240307'
 export const CFLARE_ACCOUNT_ID = '151c47aea09345bea53fef9648b8b958'
 export const ANTHROPIC_API_KEY = Deno.env.get('ANTHROPIC_API_KEY')!
 
